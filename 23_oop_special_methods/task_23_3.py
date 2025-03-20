d0056ac63213422185893fbba2277bf2376cffc5
EvgSib <giprosib@yandex.ru>
2025-03-20 07:02:27 -0500

    Add file 23.3

--- 23_oop_special_methods/task_23_3.py
+++ 23_oop_special_methods/task_23_3.py
@@ -59,7 +59,51 @@ In [10]: t2.topology
 Out[10]: {('R1', 'Eth0/4'): ('R7', 'Eth0/0'), ('R1', 'Eth0/6'): ('R9', 'Eth0/0')}
 """
 
-topology_example = {
+from pprint import pprint
+
+class Topology:
+    def __init__(self, topology_dict):
+        self.topology = self._normalize(topology_dict)
+
+    def __add__(self, other):
+        copy_topology = self.topology.copy()
+        copy_topology.update(other.topology)
+        return Topology(copy_topology)
+
+    def _normalize(self, topology_dict):
+        normalized_topology = {}
+        for box, neighbor in topology_dict.items():
+            if not neighbor in normalized_topology:
+                normalized_topology[box] = neighbor
+        return normalized_topology
+
+    def delete_link(self, from_port, to_port):
+        if from_port in self.topology and self.topology[from_port] == to_port:
+            del self.topology[from_port]
+        elif to_port in self.topology and self.topology[to_port] == from_port:
+            del self.topology[to_port]
+        else:
+            print("Такого соединения нет")
+
+    def delete_node(self, node):
+        original_size = len(self.topology)
+        for src, dest in list(self.topology.items()):
+            if node in src or node in dest:
+                del self.topology[src]
+        if original_size == len(self.topology):
+            print("Такого устройства нет")
+
+    def add_link(self, src, dest):
+        keys_and_values = set(self.topology.keys()) | set(self.topology.values())
+        if self.topology.get(src) == dest or self.topology.get(dest) == src:
+            print("Такое соединение существует")
+        elif src in keys_and_values or dest in keys_and_values:
+            print("Cоединение с одним из портов существует")
+        else:
+            self.topology[src] = dest
+
+if __name__ == "__main__":
+    topology_example = {
     ("R1", "Eth0/0"): ("SW1", "Eth0/1"),
     ("R2", "Eth0/0"): ("SW1", "Eth0/2"),
     ("R2", "Eth0/1"): ("SW2", "Eth0/11"),
@@ -71,7 +115,16 @@ topology_example = {
     ("SW1", "Eth0/3"): ("R3", "Eth0/0"),
 }
 
-topology_example2 = {
+    topology_example2 = {
     ("R1", "Eth0/4"): ("R7", "Eth0/0"),
     ("R1", "Eth0/6"): ("R9", "Eth0/0"),
 }
+    print('Экземпляр t1')
+    t1 = Topology(topology_example)
+    pprint(t1.topology)
+    print('Экземпляр t2')
+    t2 = Topology(topology_example2)
+    pprint(t2.topology)
+    print('Экземпляр t1 + t2')
+    t3 = t1 + t2
+    pprint(t3.topology)
