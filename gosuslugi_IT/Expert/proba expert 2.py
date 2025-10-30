7e0e4d0ace20652d164f292185775f7f0393cdf5
EvgSib <giprosib@yandex.ru>
2025-10-30 10:30:01 -0500

    add Lottery winnings amount & Quarterly Sales Report

---
+++ gosuslugi_IT/Expert/proba expert 2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+
+def property(func):
+    def wrapper(self):  # Важно: обёртка должна принимать `self`
+        print("Декоратор: Выполняется код перед вызовом метода.")
+        print(func(self))  # Вызов оригинального метода, передавая `self`
+        print("Декоратор: Выполняется код после вызова метода.")
+    return wrapper
+
+
+class Item:
+    def __init__(self, date, product, quantity):
+        self.date = date
+        self.product = product
+        self.quantity = int(quantity)
+
+
+    @property
+    def quarter(self):
+        month = int(self.date.split('-')[1])
+        if 1 <= month <= 3:
+            return "Q1"
+        elif 4 <= month <= 6:
+            return "Q2"
+        elif 7 <= month <= 9:
+            return "Q3"
+        else:
+            return "Q4"
+
+
+def pusk():
+    item = Item('2023-02-05', 'Шляпа', '4')
+    quarter = item.quarter
+    pass
+
+pusk()
--- gosuslugi_IT/base/proba.py
+++ gosuslugi_IT/base/proba.py
@@ -1,89 +1,173 @@
 # -*- coding: utf-8 -*-
 '''
-Пример 1 (тест):
-Входные данные:
-MCMXCIV
-
-Выходные данные:
-1994
-
-Пример 2 (тест):
-Входные данные:
-XLII
-
-Выходные данные:
-42
-
 Пример 3 (тест):
 Входные данные:
-CDXLIV
+2023-02-05:Шляпа:4;2023-03-20:Кольцо:7;2023-04-25:Браслет:6;2023-04-26:Браслет:12
 
 Выходные данные:
-444
+Q1:
+- Кольцо: 7
+- Шляпа: 4
+Q2:
+- Браслет: 18
+'''
 
-Пример 4 (тест):
-Входные данные:
-MMXX
+class Item:
+    def __init__(self, date, product, quantity):
+        self.date = date
+        self.product = product
+        self.quantity = int(quantity)
 
+
+    @property
+    def quarter(self):
+        month = int(self.date.split('-')[1])
+        if 1 <= month <= 3:
+            return "Q1"
+        elif 4 <= month <= 6:
+            return "Q2"
+        elif 7 <= month <= 9:
+            return "Q3"
+        else:
+            return "Q4"
+
+
+def generate_quarterly_report(data):
+    items_by_quarter = {}
+    # Анализ входных данных и группировка элементов по кварталам
+    for item_str in data.split(';'):
+        date, product, quantity = item_str.split(':')
+        item = Item(date, product, quantity)
+        quarter = item.quarter
+        if quarter not in items_by_quarter:
+            items_by_quarter[quarter] = {}
+        if product in items_by_quarter[quarter]:
+            items_by_quarter[quarter][product] += int(quantity)
+        else:
+            items_by_quarter[quarter][product] = int(quantity)
+
+    for key, value in sorted(items_by_quarter.items()):
+        yield f"{key}:"
+        for x, y in sorted(value.items()):
+            yield f"- {x}: {y}"
+
+# def property(func):
+#     def wrapper(self):  # Важно: обёртка должна принимать `self`
+#         func(self)  # Вызов оригинального метода, передавая `self`
+#     return wrapper
+
+input_data = '2023-02-05:Шляпа:4;2023-03-20:Кольцо:7;2023-04-25:Браслет:6;2023-04-26:Браслет:12'
+quarterly_report_generator = generate_quarterly_report(input_data)
+for report in quarterly_report_generator:
+    print(report)
+'''
 Выходные данные:
-2020
-
-Пример 5 (тест):
-Входные данные:
-DCCCXLV
-
-Выходные данные:
-845
-
-Пример 6 (тест):
-Входные данные:
-CCLXXXI
-
-Выходные данные:
-281
-
-Пример 7 (из задания):
-Входные данные:
-MCMXXCIV
-
-Выходные данные:
-1984
-
-Выходные данные:
-0,1,2
-
-Пример 8 (из задания):
-Входные данные:
-MCCXXXIV
-
-Выходные данные:
-1234
-
-если левое значение больше правого, то это окончательное число
-если левое значение меньше правого, то из правого нужно вычесть левое значение
+Q1:
+- Кольцо: 7
+- Шляпа: 4
+Q2:
+- Браслет: 18
 '''
 
 
-def roman_to_decimal(roman_numeral):
-    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+# from config import SETTINGS; SETTINGS = {'debug': False}
+# from config import SETTINGS as cfg; cfg['debug'] = False
+# import config; config.SETTINGS['debug'] = False
+# import config; config.SETTINGS.debug = False
+# import config; config.SETTINGS = {'debug': False}
+# print(config.SETTINGS['debug'])
 
-    out, mx = 0, 0
-    for cur in map(lambda c: roman_numerals[c], roman_numeral[::-1]):
-# без переворота roman_numeral = '1000  100 1000 10   100    1    5  '
-#                roman_numeral = ' V     I   C    X    M     C    M  '
-#                roman_numeral = ' 5     1  100  10  1000   100  1000'
-        if cur >= mx:
-            out = out + cur
-            mx = cur
-        else:
-            out = out - cur
-    return out
+# def my_decorator(func):
+#     def wrapper(*args, **kwargs):
+#         print("Что-то происходит до вызова функции")
+#         Вызов оригинальной функции со всеми аргументами
+#         res = func(*args, **kwargs)
+#         print("Что-то происходит после вызова функции")
+#         return res
+#     return wrapper
 
-roman_number = 'MCMXCIV'
-decimal_number = roman_to_decimal(roman_number)
-print(decimal_number)
+# @my_decorator это тоже самое, что func = my_decorator(func)
+# @my_decorator
+# def add(a, b):
+#     return a + b
+
+# result = add(5, 3)
+# print(f"Результат: {result}")
+
+# def my_decorator(func):
+#     def wrapper():
+#         print("Что-то происходит до вызова функции")
+#         func()  # Вызов оригинальной функции
+#         print("Что-то происходит после вызова функции")
+#     return wrapper
+
+# @my_decorator это тоже самое, что func = my_decorator(func)
+# @my_decorator
+# def say_hello():
+#     print("Привет, мир!")
+
+# say_hello()
+# say_hello()
+# say_hello()
 
 
+# на вход функции property подается функция quarter и возвращается тоже функция quarter
+# @property
+# def quarter(date):
+#     month = int(date.split('-')[1])
+#     if 1 <= month <= 3:
+#         return "Q1"
+#     elif 4 <= month <= 6:
+#         return "Q2"
+#     elif 7 <= month <= 9:
+#         return "Q3"
+#     else:
+#         return "Q4"
+
+# date = '2023-03-20'
+# f = property(quarter)
+# f()
+
+# def countdown(n):
+#     def inner():
+#         nonlocal n
+#         print(n)
+#         n = n-1
+#     return inner
+
+# f = countdown(5)
+# f()
+# f()
+# f()
 
 
+# def my_decorator(func):
+#     def wrapper():
+#         print("Что-то происходит до вызова функции")
+#         func()  # Вызов оригинальной функции
+#         print("Что-то происходит после вызова функции")
+#     return wrapper
+
+# def my_decorator():
+#     print('вызвана функция my_decorator')
+#     def wrapper():
+#         print("вызвана функция wrapper")
+#         a = 10
+#         b = 15
+#         return f'выводим сумму {a+b}'
+#     return wrapper
+
+# f = my_decorator()
+# print(f())
+
+# Декорирование функции вручную
+# decorated_hello = my_decorator(say_hello)
+# Вызов декорированной функции
+# decorated_hello()
+# @my_decorator
+# def say_hello():
+#     print("Привет, мир!")
+
+# Вызов декорированной функции
+# say_hello()
 
